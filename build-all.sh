a56a437b7c006271c03955a9492376f65f62befb
Derek Adair <d@derekadair.com>
2021-06-29 13:59:01 +0000

    installing silversearcher

--- build-all.sh
+++ build-all.sh
@@ -1,9 +1,15 @@
 #/bin/bash
+BASE_DIR=$PWD
 IMAGE_NAME='derekadair/python-workflow'
 docker build --no-cache -t="$IMAGE_NAME:base" .
-docker build --no-cache -t="$IMAGE_NAME:onbuild" ./onbuild
-docker build --no-cache -t="$IMAGE_NAME:dev" ./dev
-docker build --no-cache -t="$IMAGE_NAME:ffmpeg" ./ffmpeg
-docker build --no-cache -t="$IMAGE_NAME:alpine" ./alpine
-docker build --no-cache -t="$IMAGE_NAME:2.7" ./python-2.7
+cd $BASE_DIR/onbuild
+docker build --no-cache -t="$IMAGE_NAME:onbuild" .
+cd $BASE_DIR/dev
+docker build --no-cache -t="$IMAGE_NAME:dev" .
+cd $BASE_DIR/ffmpeg
+docker build --no-cache -t="$IMAGE_NAME:ffmpeg" .
+cd $BASE_DIR/alpine
+docker build --no-cache -t="$IMAGE_NAME:alpine" .
+cd $BASE_DIR/2.7
+docker build --no-cache -t="$IMAGE_NAME:2.7" .
 docker push $IMAGE_NAME
