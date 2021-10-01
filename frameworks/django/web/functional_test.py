91ce56acfa89bfb1c4130a3ac0efabc5d688ad7a
Derek Adair <d@derekadair.com>
2021-10-01 15:06:19 +0000

    Separated django example

--- frameworks/django/web/functional_test.py
+++
@@ -1,9 +0,0 @@
-from selenium import webdriver
-from selenium.webdriver.firefox.options import Options
-
-opts = Options()
-opts.headless=True
-browser = webdriver.Firefox(options=opts)
-browser.get('http://localhost:8000')
-
-assert 'Django' in browser.title
